d9d050047303e42e7346199e458be27183a2c2d1
Emir Demirli <commandiron@gmail.com>
2022-11-20 15:56:12 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import './transaction.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: "t1",
+        title: "New Shoes",
+        amount: 69.99,
+        date: DateTime.now()
+    ),
+    Transaction(
+        id: "t2",
+        title: "Weekly Groceries",
+        amount: 16.53,
+        date: DateTime.now()
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget> [
+          Card(
+            color: Colors.blue,
+            child: Container(
+              width: double.infinity,
+              child: Text("CHART!"),
+            ),
+            elevation: 5,
+          ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget> [
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 15
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Colors.black,
+                            width: 2
+                        )
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                          tx.amount.toString()
+                      ),
+                    ),
+                    Column(
+                      children: <Widget> [
+                        Text(tx.title),
+                        Text(tx.date.toString())
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
+          )
+        ],
+      )
+    );
+  }
+}
